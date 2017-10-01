23dd47eae25b4221905fb30ae26d937af7e9bc6f
Victor Hugo <vct.bta@gmail.com>
2017-10-01 11:24:43 -0300

    Usa a classe TaskController na rota 'task'

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,6 +15,26 @@ class TaskController extends Controller
         ]);
     }
 
+    public function create(Request $request)
+    {
+        $validator = \Validator::make(
+           $request->all(),
+           [
+               'name' => 'required|max:255',
+           ]
+        );
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $taskToSave = new Task;
+        $taskToSave->name = $request->get('name');
+        $taskToSave->save();
+        return redirect('/');
+    }
+
     public function remove(Task $task)
     {
         $task->delete();
--- routes/web.php
+++ routes/web.php
@@ -15,24 +15,5 @@ use App\Task;
 */
 
 Route::get('/', ['uses' => 'TaskController@index']);
-
-Route::post('/task', function(Request $request) {
-    $validator = Validator::make(
-       $request->all(),
-       [
-           'name' => 'required|max:255',
-       ]
-    );
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $taskToSave = new Task;
-    $taskToSave->name = $request->get('name');
-    $taskToSave->save();
-    return redirect('/');
-});
-
+Route::post('/task', ['uses' => 'TaskController@create']);
 Route::delete('/task/{task}', ['uses' => 'TaskController@remove']);
